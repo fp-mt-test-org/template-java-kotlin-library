0eb57e5fe937dc4dbca1bf83be95c9436c87040c
Matthew Thomas <matthewthomas@flexport.com>
2021-06-12 19:20:16 -0700

    fix: remove quotes from output.

--- {{cookiecutter.component_id}}/.ngif/battenberg-install-template.sh
+++ {{cookiecutter.component_id}}/.ngif/battenberg-install-template.sh
@@ -17,34 +17,34 @@ template_context_path="./${template_context_file}"
     fi
 
     # owner [Product Infrastructure]:
-    jq '.owner' ${template_context_path};
+    jq -r '.owner' ${template_context_path};
     sleep 1;
 
     # component_id []:
-    jq '.component_id' ${template_context_path}
+    jq -r '.component_id' ${template_context_path}
     sleep 1;
 
     # repo_owner [fp-mt-test-org]:
-    jq '.repo_owner' ${template_context_path}
+    jq -r '.repo_owner' ${template_context_path}
     sleep 1;
 
     # artifact_id [java-kotlin-lib-test-*****]:
-    jq '.artifact_id' ${template_context_path}
+    jq -r '.artifact_id' ${template_context_path}
     sleep 1;
 
     # storePath [https://github.com/fp-mt-test-org/java-kotlin-lib-test-*****]:
-    jq '.storePath' ${template_context_path}
+    jq -r '.storePath' ${template_context_path}
     sleep 1;
 
     # java_package_name [javakotlinlibtest*****]:
-    jq '.java_package_name' ${template_context_path}
+    jq -r '.java_package_name' ${template_context_path}
     sleep 1;
 
     # description [*****]:
-    jq '.description' ${template_context_path}
+    jq -r '.description' ${template_context_path}
     sleep 1;
 
     # destination [default]:
-    jq -c '.destination' ${template_context_path}
+    jq -rc '.destination' ${template_context_path}
     sleep 1;
 } | battenberg install "${github_base_url}/${template_name}"
