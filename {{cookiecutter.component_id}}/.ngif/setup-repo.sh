94ae5da1b70fb3526d774a5d90cef8e72592d9b0
Matthew Thomas <matthewthomas@flexport.com>
2021-06-12 19:06:29 -0700

    fix: Removes it from setup.

--- {{cookiecutter.component_id}}/.ngif/setup-repo.sh
+++ {{cookiecutter.component_id}}/.ngif/setup-repo.sh
@@ -9,7 +9,6 @@ git_branches=$(git branch)
 
 if ! [[ "${git_branches}" =~ "template" ]]; then
     echo "Template needs to be initialized, initializing..."
-    echo "owner_name: ${owner_name}"
     ./.ngif/initialize-template.sh
 else
     echo "Template has been previously initialized."
