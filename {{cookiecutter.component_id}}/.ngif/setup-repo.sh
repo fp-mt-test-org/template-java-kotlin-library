7e8f1cf13496a04b20fe95bc37f57cce569553ff
Matthew Thomas <matthewthomas@flexport.com>
2021-06-12 19:41:33 -0700

    fix

--- {{cookiecutter.component_id}}/.ngif/setup-repo.sh
+++ {{cookiecutter.component_id}}/.ngif/setup-repo.sh
@@ -9,11 +9,14 @@ echo "Determining if the template has been initialized..."
 git_branches=$(git branch)
 
 if ! [[ "${git_branches}" =~ "template" ]]; then
-    git checkout template
-    git checkout master
+    checkout_output=$(git checkout template || true)
 
-    echo "Template needs to be initialized, initializing..."
-    ./.ngif/initialize-template.sh
+    if [[ "${checkout_output}" =~ "error: pathspec 'template' did not match any file(s) known to git" ]]; then
+        echo "Template needs to be initialized, initializing..."
+        ./.ngif/initialize-template.sh
+    else
+        git checkout master
+    fi
 else
     echo "Template has been previously initialized."
 fi
